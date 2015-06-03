6ab4e885fd6ef4986fd06ae5035b12ebc01f8158
Lbniese <Lbniese@gmail.com>
2015-06-03 12:01:34 +0200

    Fixed Vote Kick breaking LFG queue: Fixed Vote Kick started with party in queue breaking the whole LFG queue. The status of Vote Kick is now storing in a bool variable in LfgGroupData, separated from LfgState of the group/members. If a Vote Kick started with party in queue, the members were not removed from queue correctly and would cause LFG matching system to match these "broken" players but not allowing to start a dungeon.
    *Fixed a typo in a log description too

--- src/server/game/DungeonFinding/LFGGroupData.cpp
+++ src/server/game/DungeonFinding/LFGGroupData.cpp
@@ -22,7 +22,7 @@ namespace lfg
 {
 
 LfgGroupData::LfgGroupData(): m_State(LFG_STATE_NONE), m_OldState(LFG_STATE_NONE),
-    m_Leader(), m_Dungeon(0), m_KicksLeft(LFG_GROUP_MAX_KICKS)
+	m_Leader(), m_Dungeon(0), m_KicksLeft(LFG_GROUP_MAX_KICKS), m_VoteKickActive(false)
 { }
 
 LfgGroupData::~LfgGroupData()
@@ -125,5 +125,13 @@ uint8 LfgGroupData::GetKicksLeft() const
 {
     return m_KicksLeft;
 }
+void LfgGroupData::SetVoteKick(bool active)
+ {
+	m_VoteKickActive = active;
+	}
 
+bool LfgGroupData::IsVoteKickActive() const
+ {
+	return m_VoteKickActive;
+	}
 } // namespace lfg
