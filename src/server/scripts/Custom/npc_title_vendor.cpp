543add273095e1bbc2600abb3db6eb562845fc1f
immvp <mvpberger@gmail.com>
2015-06-28 21:24:28 +0100

    npc title vendor preperation season 2

--- src/server/scripts/Custom/npc_title_vendor.cpp
+++ src/server/scripts/Custom/npc_title_vendor.cpp
@@ -1,7 +1,7 @@
 #include "ScriptPCH.h"
 #include "ItemEnchantmentMgr.h"
 #include "Language.h"
-
+ 
 #define H_X             -1258.48f
 #define H_Y             63.53f
 #define H_Z             127.9f
@@ -31,141 +31,177 @@
 #define TXT_REQ_TITLE   "you need to be at least : |TInterface/PvPRankBadges/PvPRank"
 #define TXT_MAX_RANK    "you already are max rank !"
 #define TXT_WSG_MARK    " Warsong Gulch Mark of Ranking to go! We sell the same items as the Arena Vendors do."
-
+ 
 enum Gossip_Option_Custom
 {
-	CUSTOM_OPTION_NONE = 20,
-	CUSTOM_OPTION_UNLEARN = 21,
-	CUSTOM_OPTION_EXIT = 22,
-	CUSTOM_OPTION_TITLE_PVP = 23,
-	CUSTOM_OPTION_TITLE_CLIMB = 24,
-	CUSTOM_OPTION_SPRINT = 25,
-	GOSSIP_OPTION_HELLO = 26,
-	CUSTOM_OPTION_ITEM_MENU = 27,
-	CUSTOM_OPTION_ITEM_MENU_P2 = 28,
-	CUSTOM_OPTION_ITEM_MENU_P3 = 29,
-	CUSTOM_OPTION_ITEM_MENU_P4 = 30,
-	CUSTOM_OPTION_ITEM_MENU_P5 = 31,
-	CUSTOM_OPTION_ITEM_MENU_P6 = 32,
-	CUSTOM_OPTION_ITEM_MENU_P7 = 33,
-	CUSTOM_OPTION_ITEM_MENU_MAX = 34,
-	CUSTOM_OPTION_SUFFIX = 35,
-	CUSTOM_OPTION_PROPERTY = 36,
-	CUSTOM_OPTION_VENDOR = 37,
-	CUSTOM_OPTION_MAX
+        CUSTOM_OPTION_NONE = 20,
+        CUSTOM_OPTION_UNLEARN = 21,
+        CUSTOM_OPTION_EXIT = 22,
+        CUSTOM_OPTION_TITLE_PVP = 23,
+        CUSTOM_OPTION_TITLE_CLIMB = 24,
+        CUSTOM_OPTION_SPRINT = 25,
+        GOSSIP_OPTION_HELLO = 26,
+        CUSTOM_OPTION_ITEM_MENU = 27,
+        CUSTOM_OPTION_ITEM_MENU_P2 = 28,
+        CUSTOM_OPTION_ITEM_MENU_P3 = 29,
+        CUSTOM_OPTION_ITEM_MENU_P4 = 30,
+        CUSTOM_OPTION_ITEM_MENU_P5 = 31,
+        CUSTOM_OPTION_ITEM_MENU_P6 = 32,
+        CUSTOM_OPTION_ITEM_MENU_P7 = 33,
+        CUSTOM_OPTION_ITEM_MENU_MAX = 34,
+        CUSTOM_OPTION_SUFFIX = 35,
+        CUSTOM_OPTION_PROPERTY = 36,
+        CUSTOM_OPTION_VENDOR = 37,
+        CUSTOM_OPTION_MAX
 };
-
+ 
 enum NPCs {
-	NPC_ALLIANCE_LOOK_BACK = 500113,
-	NPC_ALLIANCE_LOOK_ARROUND = 500114,
-	NPC_HORDE_LOOK_BACK = 500115,
-	NPC_HORDE_LOOK_ARROUND = 500116
+        NPC_ALLIANCE_LOOK_BACK = 500113,
+        NPC_ALLIANCE_LOOK_ARROUND = 500114,
+        NPC_HORDE_LOOK_BACK = 500115,
+        NPC_HORDE_LOOK_ARROUND = 500116
 };
-
-static const char   *titlesNames[28] =
+ 
+static char   *AtitlesNames[] =
 {
-	"Private",
-	"Corporal",
-	"Sergeant",
-	"Master Sergeant",
-	"Sergeant Major",
-	"Knight",
-	"Knight Lieutenant",
-	"Knight Captain",
-	"Knight Champion",
-	"Lieutenant Commander",
-	"Commander",
-	"Marshal",
-	"Field Marshal",
-	"Grand Marshal",
-	"Scout",
-	"Grunt",
-	"Sergeant",
-	"Senior Sergeant",
-	"First Sergeant",
-	"Stone Guard",
-	"Blood Guard",
-	"Legionnaire",
-	"Centurion",
-	"Champion",
-	"Lieutenant General",
-	"General",
-	"Warlord",
-	"High Warlord"
+        "Private",
+        "Corporal",
+        "Sergeant",
+        "Master Sergeant",
+        "Sergeant Major",
+        "Knight",
+        "Knight Lieutenant",
+        "Knight Captain",
+        "Knight Champion",
+        "Lieutenant Commander",
+        "Commander",
+        "Marshal",
+        "Field Marshal",
+        "Grand Marshal",
+        "Battlemaster"
 };
-
-
+ 
+static char *HtitlesNames[] =
+{
+        "Scout",
+        "Grunt",
+        "Sergeant",
+        "Senior Sergeant",
+        "First Sergeant",
+        "Stone Guard",
+        "Blood Guard",
+        "Legionnaire",
+        "Centurion",
+        "Champion",
+        "Lieutenant General",
+        "General",
+        "Warlord",
+        "High Warlord",
+        "Battlemaster"
+};
+ 
+static int *titlesCost[] =
+{
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15
+       
+};
+ 
 static uint32 GetTotalTokens(Player* Player)
 {
-	//intialize an item
-	Item*           pItem;
-	//loop in the currencytoken slots
-	for (uint8 i = CURRENCYTOKEN_SLOT_START; i < CURRENCYTOKEN_SLOT_END; ++i)
-	{
-		//try to set or item to the one from this slot
-		pItem = Player->GetItemByPos(INVENTORY_SLOT_BAG_0, i);
-		//if player have an item in this slot and his entry is the same has our token return the count
-		if (pItem && pItem->GetEntry() == TOKEN)
-			return (pItem->GetCount());
-	}
-	return (0);
+        //intialize an item
+        Item*           pItem;
+        //loop in the currencytoken slots
+        for (uint8 i = CURRENCYTOKEN_SLOT_START; i < CURRENCYTOKEN_SLOT_END; ++i)
+        {
+                //try to set or item to the one from this slot
+                pItem = Player->GetItemByPos(INVENTORY_SLOT_BAG_0, i);
+                //if player have an item in this slot and his entry is the same has our token return the count
+                if (pItem && pItem->GetEntry() == TOKEN)
+                        return (pItem->GetCount());
+        }
+        return (0);
 }
-
+ 
 class npc_title_giver : public CreatureScript
 {
 public:
-	npc_title_giver() : CreatureScript("npc_title_giver") {}
-
-	void RewardTitles(Player *player, uint8 *nextTitle, uint16 *reqTokens, const uint16 totalTokens, const uint8 faction)
-	{
-		while ((*nextTitle < 14) && (((*reqTokens += (1 + *nextTitle) * TOKEN_COUNT)) <= totalTokens))
-		{
-			(*nextTitle)++;
-			player->SetTitle(sCharTitlesStore.LookupEntry(*nextTitle + faction));
-		}
-	}
-
-	const char *GetNextTitleName(const uint8 nextTitle, const uint16 reqTokens, const uint16 totalTokens, const uint8 faction)
-	{
-		std::ostringstream  ss;
-
-		if (nextTitle > 13)
-			ss << TXT_MAX_RANK;
-		else
-		{
-			ss << TXT_NEXT_TITLE;
-			if (nextTitle < 9)
-				ss << '0';
-			ss << 1 + nextTitle << FORMAT_END << titlesNames[nextTitle + faction];
-			ss << " in " << reqTokens - totalTokens << " tokens.";
-		}
-		return (ss.str().c_str());
-	}
-
-	bool OnGossipHello(Player* player, Creature* me)
-	{
-		const uint16  totalTokens = GetTotalTokens(player);
-		const uint8   faction = (player->GetTeam() == ALLIANCE) ? 0 : 14;
-		uint8         nextTitle = 0;
-		uint16        reqTokens = 0;
-
-		RewardTitles(player, &nextTitle, &reqTokens, totalTokens, faction);
-		const char *gossipText = GetNextTitleName(nextTitle, reqTokens, totalTokens, faction);
-		player->PlayerTalkClass->ClearMenus();
-		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, gossipText, GOSSIP_SENDER_MAIN, CUSTOM_OPTION_EXIT);
-		player->ADD_GOSSIP_ITEM(1, TXT_KTHXBY, GOSSIP_SENDER_MAIN, CUSTOM_OPTION_EXIT);
-		player->PlayerTalkClass->SendGossipMenu(9425, me->GetGUID());
-		return (true);
-	}
-
-	bool OnGossipSelect(Player* player, Creature* /*me*/, uint32 /*uiSender*/, uint32 /*uiAction*/)
-	{
-		player->PlayerTalkClass->SendCloseGossip();
-		return (true);
-	}
+        npc_title_giver() : CreatureScript("npc_title_giver") {}
+ 
+        void RewardTitles(Player *player, uint8 *nextTitle, uint16 *reqTokens, const uint16 totalTokens, const uint8 faction)
+        {
+                while ((*nextTitle < 15) && (titlesCost[*nextTitle]) <= totalTokens))
+                {
+                        (*nextTitle)++;
+                        if(*nextTitle == 14)
+                                player->SetTitle(sCharTitlesStore.LookupEntry(72));
+                        else
+                                player->SetTitle(sCharTitlesStore.LookupEntry(*nextTitle + faction));
+                }
+        }
+ 
+        const char *GetNextTitleName(const uint8 nextTitle, const uint16 reqTokens, const uint16 totalTokens, Player* player)
+        {
+                std::ostringstream  ss;
+ 
+                if (nextTitle > 14)
+                        ss << TXT_MAX_RANK;
+                else
+                {
+                        ss << TXT_NEXT_TITLE;
+                        if (nextTitle < 9)
+                                ss << '0';
+                        if(player->GetFactionTeam == TEAM_ALLIANCE)
+                        {
+                                ss << 1 + nextTitle << FORMAT_END << AtitlesNames[nextTitle];
+                        ss << " in " << reqTokens - totalTokens << " tokens.";
+                        }
+                        else
+                        {
+                        ss << 1 + nextTitle << FORMAT_END << HtitlesNames[nextTitle];
+                        ss << " in " << reqTokens - totalTokens << " tokens.";
+                        }
+                }
+                return (ss.str().c_str());
+        }
+ 
+        bool OnGossipHello(Player* player, Creature* me)
+        {
+                const uint16  totalTokens = GetTotalTokens(player);
+                const uint8   faction = (player->GetTeam() == ALLIANCE) ? 0 : 14;
+                uint8         nextTitle = 0;
+                uint16        reqTokens = 0;
+ 
+                RewardTitles(player, &nextTitle, &reqTokens, totalTokens, faction);
+                const char *gossipText = GetNextTitleName(nextTitle, reqTokens, totalTokens, player);
+                player->PlayerTalkClass->ClearMenus();
+                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, gossipText, GOSSIP_SENDER_MAIN, CUSTOM_OPTION_EXIT);
+                player->ADD_GOSSIP_ITEM(1, TXT_KTHXBY, GOSSIP_SENDER_MAIN, CUSTOM_OPTION_EXIT);
+                player->PlayerTalkClass->SendGossipMenu(9425, me->GetGUID());
+                return (true);
+        }
+ 
+        bool OnGossipSelect(Player* player, Creature* /*me*/, uint32 /*uiSender*/, uint32 /*uiAction*/)
+        {
+                player->PlayerTalkClass->SendCloseGossip();
+                return (true);
+        }
 };
 void AddSC_title_system()
 {
-	new npc_title_giver();
-
+        new npc_title_giver();
+ 
 }
