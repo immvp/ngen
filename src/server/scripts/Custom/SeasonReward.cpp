417bcbda1851fc81113db2a1fd563a466d676fc7
immvp <mvpberger@gmail.com>
2015-06-30 19:16:58 +0100

    1 more

--- src/server/scripts/Custom/SeasonReward.cpp
+++ src/server/scripts/Custom/SeasonReward.cpp
@@ -7,57 +7,46 @@ public:
 
 	bool OnGossipHello(Player* player, Creature* creature) override
 	{
-		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Reward", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);			
-		player->SEND_GOSSIP_MENU(1, creature->GetGUID());
-		return true;
-	}
-
-	bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 actions) override
-	{
-		if (actions == GOSSIP_ACTION_INFO_DEF + 1)
+		if (player->GetGUID() == 1746 || player->GetGUID() == 2135 || player->GetGUID() == 2702 || player->GetGUID() == 2705) // Rank 1's
 		{
-			if (player->GetGUID() == 1746 || player->GetGUID() == 2135 || player->GetGUID() == 2702 || player->GetGUID() == 2705) // Rank 1's
-			{
-				uint32 achievementId = 419;
-				if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
-					player->CompletedAchievement(achievementEntry);
-				player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Rank 1 this season!");
-			}
-			else if (player->GetGUID() == 39 || player->GetGUID() == 464) // Gladiator's
-			{
-				uint32 achievementId = 2091;
-				if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
-					player->CompletedAchievement(achievementEntry);
-				player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Gladiator this season!");
-			}
-			else if (player->GetGUID() == 1913 || player->GetGUID() == 2015) // Dualist's
-			{
-				uint32 achievementId = 2092;
-				if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
-					player->CompletedAchievement(achievementEntry);
-				player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Duelist this season!");
-			}
-			else if (player->GetGUID() == 9 || player->GetGUID() == 1418 || player->GetGUID() == 1926 || player->GetGUID() == 1928 || player->GetGUID() == 47) // Rival's
-			{
-				uint32 achievementId = 2093;
-				if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
-					player->CompletedAchievement(achievementEntry);
-				player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Rival this season!");
-			}
-			else if (player->GetGUID() == 2388 || player->GetGUID() == 3436 || player->GetGUID() == 1261 || player->GetGUID() == 1726 || player->GetGUID() == 2038 || player->GetGUID() == 1923) // Rival's
-			{
-				uint32 achievementId = 2090;
-				if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
-					player->CompletedAchievement(achievementEntry);
-				player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Challenger this season!");
-			}
-			else
-			{
-				player->GetSession()->SendAreaTriggerMessage("You did not meet this season's requirements for rewards. Good luck next season!");
-			}
+			uint32 achievementId = 419;
+			if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
+				player->CompletedAchievement(achievementEntry);
+			player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Rank 1 this season!");
+		}
+		else if (player->GetGUID() == 39 || player->GetGUID() == 464) // Gladiator's
+		{
+			uint32 achievementId = 2091;
+			if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
+				player->CompletedAchievement(achievementEntry);
+			player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Gladiator this season!");
+		}
+		else if (player->GetGUID() == 1913 || player->GetGUID() == 2015) // Dualist's
+		{
+			uint32 achievementId = 2092;
+			if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
+				player->CompletedAchievement(achievementEntry);
+			player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Duelist this season!");
+		}
+		else if (player->GetGUID() == 9 || player->GetGUID() == 1418 || player->GetGUID() == 1926 || player->GetGUID() == 1928 || player->GetGUID() == 47) // Rival's
+		{
+			uint32 achievementId = 2093;
+			if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
+				player->CompletedAchievement(achievementEntry);
+			player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Rival this season!");
+		}
+		else if (player->GetGUID() == 2388 || player->GetGUID() == 3436 || player->GetGUID() == 1261 || player->GetGUID() == 1726 || player->GetGUID() == 2038 || player->GetGUID() == 1923) // Rival's
+		{
+			uint32 achievementId = 2090;
+			if (AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievementId))
+				player->CompletedAchievement(achievementEntry);
+			player->GetSession()->SendAreaTriggerMessage("Congratulations on achieving Challenger this season!");
+		}
+		else
+		{
+			player->GetSession()->SendAreaTriggerMessage("You did not meet this season's requirements for rewards. Good luck next season!");
 		}
 		player->CLOSE_GOSSIP_MENU();
-
 		return true;
 	}
 };
