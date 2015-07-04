10168ab7a4ce15cb2dc62f6fca92558eb04c20ce
Nick <nickiaj@live.dk>
2015-07-04 18:27:16 +0200

    Fiiix

--- src/server/scripts/Custom/r15transmog.cpp
+++ src/server/scripts/Custom/r15transmog.cpp
@@ -3,14 +3,27 @@ class r15transmog : public PlayerScript
 public:
 	r15transmog() : PlayerScript("r15transmog") { }
 
-	void OnLogin(Player* player)
+	class Transmog : public BasicEvent
 	{
-		if (player->GetTotalTokens(player) >= 1480)
+	public:
+		Transmog(Player* player) : player(player) {}
+
+		bool Execute(uint64 /*time*/, uint32 /*diff*/)
 		{
 			for (int i = EQUIPMENT_SLOT_START; i < EQUIPMENT_SLOT_END; i++)
 			{
 				player->SetVisibleItemSlot(i, player->GetItemByPos(i));
 			}
+			return true;
+		}
+		Player* player;
+	};
+
+	void OnLogin(Player* player)
+	{
+		if (player->GetTotalTokens(player) >= 1480)
+		{
+			player->m_Events.AddEvent(new Transmog(player), player->m_Events.CalculateTime(5000));
 		}
 	}
 };
