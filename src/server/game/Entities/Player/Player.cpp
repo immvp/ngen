b7f14a429128297e3f715dd3ded1df93d279cbde
Nick <nickiaj@live.dk>
2015-07-11 01:58:37 +0200

    fix for noob transmog crash

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -12586,11 +12586,15 @@ void Player::SetVisibleItemSlot(uint8 slot, Item* pItem)
 
 				SetUInt32Value(PLAYER_VISIBLE_ITEM_1_ENTRYID + (slot * 2), item_2);
 			}
-			else
+			else if (class_t)
 			{
 				Field* class_tfield = class_t->Fetch();
 				uint32 item_2 = class_tfield[0].GetUInt32();
 				SetUInt32Value(PLAYER_VISIBLE_ITEM_1_ENTRYID + (slot * 2), item_2);
+			}
+			else
+			{
+				SetUInt32Value(PLAYER_VISIBLE_ITEM_1_ENTRYID + (slot * 2), pItem->GetEntry());
 			}
 		}
 		else if (class_t)
